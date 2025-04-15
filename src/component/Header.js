ce78d946057949a217c49de9cadd965babd3caa2
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-04-15 14:26:56 +0545

    Swiggy

---
+++ src/component/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="p-[15px] shadow-xl bg-red-950">
+        <div className="max-w [1200px] mx-auto flex items-center">
+          <div className="w-[100px]">
+            <img
+              src={require("../image/Swiggy_logo.png")}
+              alt="Logo"
+              className=""
+            />
+          </div>
+          <div className="">
+            kathmandu,Nepal
+
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
